f76a18d0c04de0721d06edc20580b9fac8e7ed32
sivasathiya <sathyasiva369@gmail.com>
2017-06-27 15:44:49 +0530

    Create d.cpp

---
+++ d.cpp
@@ -0,0 +1,17 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+char a;
+cin>>a;
+if(a==a||a==A||a==e||a==E||a==i||a==I||a==o||a==O||a==u||a==U)
+{
+cout<<"vowels";
+}
+else
+{
+cout<<"consonent";
+}
+getch();
+return 0;
+}
